6b66134ac78530933c2521c3822136cf53d5ed96
Lawal Habib <habiblass2001@gmail.com>
2025-09-27 23:56:37 +0100

    edit page layout and configurations

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+	reactStrictMode: true,
+	compiler: {
+		// Enables the SWC styled-components transform for better SSR & display names
+		styledComponents: true,
+	},
 };
 
 export default nextConfig;
